7f533f9715cdd93e65e426ffea8d5830a46e4804
Nicomini10 <48324282+Nicomini10@users.noreply.github.com>
2020-03-04 19:18:22 +0100

    aggiunto scarica calendario ligue 1

--- app/src/main/java/com/example/test_news/Ligue1/CalendarioLigue1.java
+++ app/src/main/java/com/example/test_news/Ligue1/CalendarioLigue1.java
@@ -2,11 +2,14 @@ package com.example.test_news.Ligue1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.test_news.R;
-import com.example.test_news.SerieA.fecthDataCalendarioSerieA;
+import com.example.test_news.ScaricaCalendarioLigue1.ScaricaCalendarioLigue1Activity;
 
 public class CalendarioLigue1 extends AppCompatActivity {
 
@@ -18,6 +21,15 @@ public class CalendarioLigue1 extends AppCompatActivity {
         setContentView(R.layout.activity_calendario_ligue1);
 
         data = (TextView) findViewById(R.id.fectdata);
+        Button button = (Button) findViewById(R.id.ScaricaCalendario);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ScaricaCalendarioLigue1Activity.class);
+                startActivity(intent);
+            }
+        });
 
         fecthDataCalendarioLigue1 process = new fecthDataCalendarioLigue1();
         process.execute();
